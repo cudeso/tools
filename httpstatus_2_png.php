61b2fff5c9663c8f3ff01a4186689ac0ab3b3eed
Koen Van Impe <koen.vanimpe@cudeso.be>
2012-10-09 22:33:24 +0200

    "ignore_ip" option; rewrite overflow for circle width

--- httpstatus_2_png.php
+++ httpstatus_2_png.php
@@ -6,8 +6,9 @@
  *	Inspiration from http://www.seehuhn.de/pages/internet
  *	As seen in HDMoore http://www.youtube.com/watch?v=b-uPh99whw4
  *
- *	@param 	filename 	the log file to parse
- *	@param	type			what type of graph to create, based on http response code (20, 40, 50)
+ *	@param 	logfile 								the log file to parse
+ *	@param	http_response_code			what type of graph to create, based on http response code (20, 40, 50)
+ *	@param	ignore_ips							(optional, what IPs to ignore)
  *
  *	@version 20121009
  * 	@author Koen Van Impe <koen.vanimpe@cudeso.be>
@@ -18,24 +19,27 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 // Check for the correct numbers of paramters; display usage
-if ($argc != 3) {
+if (!($argc == 3 or $argc == 4)) {
 	?>
 Create a graph based on the first 8 bits of an IPv4 address
   
 Usage: 
- php <?php echo $argv[0]; ?> <logfile> <http_response_code> > <image>
+ php <?php echo $argv[0]; ?> <logfile> <http_response_code> [ignore_ips] > <image>
   logfile	:	full path of the logfile to parse
   http_response_code : what http response code to plot (20x, 40x or 50x)
+  [ignore_ips] : list of IPs to ignore (notation: '192,10,172')
   image : image to export to, fe. image.png
 
 Example:
- php <?php echo $argv[0]; ?> access.log 40x > image.png
+ php <?php echo $argv[0]; ?> access.log 40x '192,172' > image.png
 	<?php
 	die();
 }
 
 // Variable init
 $ip_array = array();
+$qt_array = array();
+$ip_ignore_array = array();
 $logfile = $argv[1];
 $color_param = (int) $argv[2]; // strip the x from 50x
 $ip = 0;
@@ -43,6 +47,7 @@ $ip = 0;
 // Define graph settings
 $basegraph_x = 800;
 $basegraph_y = 800;
+$max_width_circle = 85;
 
 $im = ImageCreate($basegraph_x,$basegraph_y);
 $background = ImageColorAllocate($im,0xa5,0x9a,0x7e);
@@ -56,6 +61,11 @@ if ($color_param == "50") $color = $blue;
 elseif ($color_param == "40") $color = $red;
 else $color = $green;
 
+// IPs to ignore
+if (strlen($argv[3]) > 0) {
+	$ip_ignore_array = explode(",", $argv[3]);
+}
+
 // Execute the command, save the output, then walk through the output
 $color_param_esc = "^".$color_param;
 exec("cat " . escapeshellarg($logfile) . " | awk '{ print $9 \" \" $1; }' |grep " . escapeshellarg($color_param_esc), $output);
@@ -65,17 +75,20 @@ if (is_array($output) and count($output) > 0) {
 			$arr_line = explode(" ", $line);
 			
 			// Build up the array width future "width" of the circles
-			$ipstr = substr($arr_line[1], 0, strpos($arr_line[1], ".") );
-			if ($ip_array[$ipstr]["qt"] == 0) $ip_array[$ipstr]["qt"] = 3;		// Minimal width
-			else if ($ip_array[$ipstr]["qt"] > 85) {
-				for ($overflow=1;$overflow<=255;$overflow++) {
-					$ip_array[$ipstr]["qt"] = round($ip_array[$ipstr]["qt"] / 2);
-				}
-			}		
-			else $ip_array[$ipstr]["qt"] = $ip_array[$ipstr]["qt"] + 1;			
+			if (!(strpos($arr_line[1], ":") > 0)) {	// ignore IPv6 values
+				$ipstr = substr($arr_line[1], 0, strpos($arr_line[1], ".") );
+				// check if it's in the ignore list
+				if (!(in_array($ipstr, $ip_ignore_array))) $ip_array[$ipstr]["qt"] = $ip_array[$ipstr]["qt"] + 1;							
+			}
 		}
 	}			
 }
+
+// Get the max of the array and recalculate the width of the circles
+foreach($ip_array as $key => $val) { $qt_array[] = $val["qt"]; }
+if (max($qt_array) > $max_width_circle)	$overflow_value = round( max($qt_array) / $max_width_circle);
+else $overflow_value = 1;
+for($x = 1; $x<= 255; $x++) { $ip_array[$x]["qt"] = round($ip_array[$x]["qt"] / $overflow_value); }
 
 // Walk through the array, set the coordinates and labels
 for($y=0;$y<=15;$y++) {
