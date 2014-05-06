a93f1bf48a4fb90169706cd4d963cbe800026c0c
Koen Van Impe <koen.vanimpe@cudeso.be>
2014-05-06 23:01:40 +0200

    MIME text sending + capture connect time out
    Added MIME text mail
    Capture a connection time out error

--- ceds.py
+++ ceds.py
@@ -14,12 +14,13 @@
 from OpenSSL import SSL
 import socket, datetime
 import smtplib
+from email.mime.text import MIMEText
 
 servers_to_check = "ceds.checks"
 alert_days = 5
 mail_rcpt = "<>"
 mail_from = "<>"
-mail_server = "localhost"
+mail_server = "127.0.0.1"
 
 servers = open( servers_to_check, "r")
 cur_date = datetime.datetime.utcnow()
@@ -33,29 +34,36 @@ for line in servers:
         context = SSL.Context(SSL.SSLv23_METHOD)
         sock = SSL.Connection(context, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 
-        sock.connect( (str(host) , int(port)) )
-        sock.send("\x00")       # Send empty to trigger response
-        get_peer_cert=sock.get_peer_certificate()
-        sock.close()
+        try:
+            sock.connect( (str(host) , int(port)) )
+            sock.send("\x00")       # Send empty to trigger response
+            get_peer_cert=sock.get_peer_certificate()
+            sock.close()
 
-        exp_date =  datetime.datetime.strptime(get_peer_cert.get_notAfter(),'%Y%m%d%H%M%SZ')        
-        days_to_expire = int((exp_date - cur_date).days)
-        cert_tested = cert_tested + 1
+            exp_date =  datetime.datetime.strptime(get_peer_cert.get_notAfter(),'%Y%m%d%H%M%SZ')        
+            days_to_expire = int((exp_date - cur_date).days)
+            cert_tested = cert_tested + 1
 
-        if days_to_expire < 0:
-            response = response + "\n " + host + ":" + port + " EXPIRED"
-        elif alert_days > days_to_expire:
-            response = response + "\n " + host + ":" + port + " expires in " + str(days_to_expire) + " days"
-        #else:
-            #response = response + "\n " + host + ":" + port + " OK"
+            if days_to_expire < 0:
+                response = response + "\n %s : %s EXPIRED" % (host, port)
+            elif alert_days > days_to_expire:
+                response = response + "\n %s : %s expires in %s dayes " % (host, port, days_to_expire)
+            #else:
+                #response = response + "\n %s : %s OK" % (host,port)
+        except:
+            response = response + "\n Unable to connect to %s : %s " % (host, port)
     except SSL.Error,e:
         print e
 
 if response:
-    response = "From: " + mail_from + "\nSubject: Certificate check " + str(cur_date) + " \n \n \nCertificate check " + str(cur_date) + "\n-----------------------------------------------\n" + response
-    response = response + "\n\nTotal certificates tested : " + str(cert_tested) + "\n"
+    response = response + "\n\nTotal certificates tested : %s \n" % cert_tested
     try:
-       smtpObj = smtplib.SMTP( mail_server )
-       smtpObj.sendmail(mail_from, mail_rcpt, response)         
+        message = MIMEText( response )
+        message["Subject"] = "Certificate check %s " % cur_date
+        message["From"] = mail_from
+        message["To"] = mail_rcpt
+        smtpObj = smtplib.SMTP( mail_server )
+        smtpObj.sendmail(mail_from, mail_rcpt, message.as_string())
+        smtpObj.quit()
     except smtplib.SMTPException:
         print "Unable to send mail"
