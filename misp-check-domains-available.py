0d18305ef37ed4fb78846afa074f63a4ccc9d570
Koen Van Impe <koen.vanimpe@cudeso.be>
2020-05-23 17:54:04 +0200

    MISP - Check if domain available for registration
    Check if domains found in threat events of MISP are available for
    registration.
    Use case: register domains to setup sinkhole.

---
+++ misp-check-domains-available.py
@@ -0,0 +1,122 @@
+import json
+import requests
+import re
+import whois
+
+from pyfaup.faup import Faup
+import sys
+import codecs
+
+import smtplib
+
+'''
+    Check if domains found in threat events of MISP are available for registration.
+    Use case: register domains to setup sinkhole
+
+    Koen Van Impe
+
+'''
+
+key='MISP-API'
+url='MISP-SERVER'
+timeframe='1d'
+ignore_eventid = [  ]
+
+
+def checkDomain(domain):
+    response = requests.get("https://dns.google/resolve?name={}".format(domain))
+    try:
+        response_json = json.loads(response.text)
+        if response_json['Status'] != 0:
+            return True
+        return False
+    except:
+        return False
+
+
+def getmisp_domains(key, url, timeframe):
+    response_domains = []
+    headers = { 'Authorization': '{}'.format(key), 'Content-type': 'application/json' , 'Accept': 'application/json'}
+    payload = '{ "returnFormat": "json", "type": "domain", "last": "%s", "enforceWarninglist": true }' % timeframe
+    response = requests.post(url, headers=headers, data=payload, verify=False)
+    json_response = json.loads(response.text)
+    try:
+        for attr in json_response['response']['Attribute']:
+            url = attr['value']
+            eventid = attr['event_id']
+            if eventid not in ignore_eventid:
+                category = attr['category']
+                timestamp = attr['timestamp']
+                response_domains.append({'domain': domain, 'eventid': eventid, 'category': category, 'timestamp': timestamp})
+        return response_domains
+    except:
+        return response_domains
+
+
+def getmisp_urls(key, url, timeframe):
+    response_domains = []
+    headers = { 'Authorization': '{}'.format(key), 'Content-type': 'application/json' , 'Accept': 'application/json'}
+    payload = '{ "returnFormat": "json", "type": "url", "last": "%s", "enforceWarninglist": true }' % timeframe
+    response = requests.post(url, headers=headers, data=payload, verify=False)
+    json_response = json.loads(response.text)
+    fp = Faup()
+    try:
+        for attr in json_response['response']['Attribute']:
+            url = attr['value']
+            eventid = attr['event_id']
+            if eventid not in ignore_eventid:
+                category = attr['category']
+                timestamp = attr['timestamp']
+                fp.decode(url)
+                domain = fp.get_domain()
+                if not re.match(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", domain):
+                    response_domains.append({'domain': domain, 'eventid': eventid, 'category': category, 'timestamp': timestamp})
+
+        return response_domains
+    except:
+        return response_domains 
+
+
+def inCheckRegister(domain, domainlist):
+    for el in domainlist:
+        if domain == el['domain']:
+            return True
+    return False
+
+
+def checkRegister(domains):
+    check_to_register = []
+    if len(domains) > 0:
+        for domain in domains:
+            # First check the DNS
+            if not inCheckRegister(domain['domain'], check_to_register) and checkDomain(domain['domain']):
+                # Now check whois
+                try:
+                    whois_result = whois.query(domain['domain'])
+                    if whois_result.creation_date is None:
+                        check_to_register.append({'domain': domain['domain'], 'eventid': domain['eventid'], 'reason': 'No DNS. No Whois', 'category': domain['category'], 'timestamp': domain['timestamp'] })
+                except ValueError: 
+                    continue
+                except Exception as e:
+                    reason = str(e).split('\n')[0]
+                    check_to_register.append({'domain': domain['domain'], 'eventid': domain['eventid'], 'reason': reason,  'category': domain['category'], 'timestamp': domain['timestamp'] })
+
+    return check_to_register
+
+
+# Read all domains
+res_urls = checkRegister(getmisp_urls(key, url, timeframe))
+res_domains = checkRegister(getmisp_domains(key, url, timeframe))
+
+message="Subject: MISP Domains available for registration\n\n\n"
+if len(res_urls) > 0:
+    for domain in res_urls:
+        message = message + json.dumps(domain) + "\n"
+if len(res_domains) > 0:
+    for domain in res_domains:
+        message = message + json.dumps(domain) + "\n"
+
+smtp_server='127.0.0.1'
+sender_email='MAIL_SENDER_RCPT'
+with smtplib.SMTP(smtp_server) as server:
+    server.sendmail(sender_email, sender_email, message)
