941e55ab6df41897b8f12a9bc5a987366e7cab67
Koen Van Impe <koen.vanimpe@cudeso.be>
2022-01-12 15:21:50 +0100

    Fix VMs with spaces in name

--- vmware-backup/backup-vms.sh
+++ vmware-backup/backup-vms.sh
@@ -11,23 +11,23 @@ vm_backup()
 	VM=$1
 
 	# Create directory for backup
-	mkdir -p $DST/$VM
-	rm -f $DST/$VM/*
+	mkdir -p "$DST/$VM"
+	rm -f "$DST/$VM/"*
 
 	# Copy base files
-	cp -p $SRC/$VM/*.vmx $DST/$VM/
-	cp -p $SRC/$VM/*.nvram $DST/$VM/
-	cp -p $SRC/$VM/*.vmsd $DST/$VM/
-	cp -p $SRC/$VM/*.log $DST/$VM/
+	cp -p "$SRC/$VM/"*.vmx "$DST/$VM/"
+	cp -p "$SRC/$VM/"*.nvram "$DST/$VM/"
+	cp -p "$SRC/$VM/"*.vmsd "$DST/$VM/"
+	cp -p "$SRC/$VM/"*.log "$DST/$VM/"
 
 	# Create a new snapshot
-	vmid=$(vim-cmd vmsvc/getallvms | grep $VM | awk '{print $1}')
+	vmid=$(vim-cmd vmsvc/getallvms | grep "$VM" | awk '{print $1}')
 	vim-cmd vmsvc/snapshot.create $vmid backup 'Snapshot created by Backup Script' 0 0
 
 	# Backup (and shrink) the disk
-	for f in $(find $SRC/$VM/*.vmdk); do
-  		base=$(basename $f)
-  		vmkfstools -i $f $DST/$VM/$base -d thin
+	find "$SRC/$VM/"*.vmdk  -type f | while read -r f ; do
+  		base=$(basename "$f")
+  		vmkfstools -i "$f" "$DST/$VM/$base" -d thin
 	done
 
 	# Remove the snapshot
@@ -35,12 +35,13 @@ vm_backup()
 	vim-cmd vmsvc/snapshot.remove $vmid $vmsnapshotid
 
 	# Create a tar archive
-	tar -zcf $DST/$VM.tar.gz $DST/$VM/*
+	tar -zcf "$DST/$VM.tar.gz" "$DST/$VM/*"
 }
 
-for f in $(find $SRC/ -type d); do
-	vm=$(basename $f)
-	if [ -n "$vm" ] && [ "$vm" != ".sdd.sf" ] && [ "$vm" != "datastore1" ]; then
-		vm_backup $vm
+find "$SRC/" -type d | while read -r f ; do
+	vm_tmp=$(basename "$f")
+	vm="$vm_tmp"
+	if [ -n "$vm" ] && [ "$vm" != ".sdd.sf" ] && [ "$vm" != "datastore1" ] && [ "$vm" != "images" ]; then
+		vm_backup "$vm"
 	fi
 done
