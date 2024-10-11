dce50b8b58c5e7dbe34b51581658f19c1860559f
Koen Van Impe <koen.vanimpe@cudeso.be>
2024-10-11 13:33:02 +0200

    Update parse_ddosia.py

--- ddosia-extract/parse_ddosia.py
+++ ddosia-extract/parse_ddosia.py
@@ -18,6 +18,8 @@ misp_url = "MISP"
 misp_key = "KEY"
 misp_verifycert = True
 mattermost_hook = ""
+teams_hook = ""
+ddosia_file_output = "/var/www/MISP/app/webroot/misp-export/ddosia.txt"
 
 # Output
 target_hostnames = []
@@ -26,8 +28,17 @@ target_domains = []
 # Send to Mattermost?
 send_mattermost = False
 
+# Send to Teams
+send_teams = False
+
+# Write to file
+write_to_ddosia_file_output = False
+
 # MISP organisation "witha.name"
 query_org = "ae763844-03bf-4588-af75-932d5ed2df8c"
+
+# Published?
+published = True
 
 # Limit for recent events
 date_filter = "1d"
@@ -37,7 +48,7 @@ misp = PyMISP(misp_url, misp_key, misp_verifycert)
 print(f"Extract hostnames from {misp_url}")
 
 # Search for events
-events = misp.search("events", pythonify=True, org=query_org, published=True, date=date_filter)
+events = misp.search("events", pythonify=True, org=query_org, published=published, date=date_filter)
 
 # Process events
 if len(events) > 0:
@@ -61,29 +72,55 @@ if len(events) > 0:
     if len(target_hostnames) > 0:
         target_hostnames.sort()
         target_domains.sort()
-        now = datetime.now()
+        
+        title = "DDoSia config: Parsed {} MISP events and found {} unique hostnames for {} domains - ({}, last {})".format(len(events), len(target_hostnames), len(target_domains), datetime.now().date(), date_filter)
+        summary = "Hostnames\n------------\n"
+        summary_md = "# Hostnames\n"
 
-        print("Parsed {} events and found {} unique hostnames for {} domains - ({})".format(len(events), len(target_hostnames), len(target_domains), now.date()))
-        if send_mattermost:
-            summary = "# Parsed {} events and found {} unique hostnames for {} domains - ({})\n\n## Hostnames\n\n".format(len(events), len(target_hostnames), len(target_domains), now.date())
-
-        print("Hostnames")
         for t in target_hostnames:
-            if send_mattermost:
-                summary += "\n{}".format(t)
-            print(f" {t}")
+            summary += "\n{}".format(t)
+            summary_md += "\n- {}".format(t)
 
-        print("Domains")
-        if send_mattermost:
-            summary += "\n## Domains\n\n"
+        summary += "\n\nDomains\n----------\n"
+        summary_md += "\n\n# Domains\n"
         for t in target_domains:
-            if send_mattermost:
-                summary += "\n{}".format(t)
-            print(f" {t}")
-
+            summary += "\n{}".format(t)
+            summary_md += "\n- {}".format(t)
+        summary_md += "\n"
+        
         if send_mattermost:
-            summary += "\n"
-            message = {"username": "witha.name-reporters", "text": summary}
+            summary_md = title + summary_md + "\n"
+            message = {"username": "witha.name-reporters", "text": summary_md}
             r = requests.post(mattermost_hook, data=json.dumps(message))
+            print(r, r.status_code, r.text)
+                        
+        if send_teams:
+            message = {
+                    "type": "message",
+                    "attachments": [
+                        {
+                            "contentType": "application/vnd.microsoft.teams.card.o365connector",
+                            "content": {
+                                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                                "type": "MessageCard",
+                                "context": "https://schema.org/extensions",
+                                "title": title,
+                                "version": "1.0",
+                                "sections": [
+                                    {
+                                        "text": summary_md
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            r = requests.post(teams_hook, json=message)
+            
+        if write_to_ddosia_file_output:
+            summary = title + "\n\n" + summary + "\n"
+            with open(ddosia_file_output, 'w') as file:
+                file.write(summary)
+
 else:
     print("No events found.")
